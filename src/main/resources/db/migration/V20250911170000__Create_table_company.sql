c51f6c4023e9c0152866e907f10addeef1b8a5ae
Andy Hui <chhuiwork@gmail.com>
2025-09-11 21:36:44 +0800

    Feat: Configure flyway and add migrations files

---
+++ src/main/resources/db/migration/V20250911170000__Create_table_company.sql
@@ -0,0 +1,5 @@
+CREATE TABLE t_company(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    PRIMARY KEY(id)
+);
---
+++ src/main/resources/db/migration/V20250911213000__Create_table_employee.sql
@@ -0,0 +1,10 @@
+CREATE TABLE t_employee(
+   id BIGINT NOT NULL AUTO_INCREMENT,
+   name VARCHAR(255),
+   age INT,
+   salary FLOAT,
+   status BOOLEAN,
+   company_id BIGINT,
+   PRIMARY KEY(id),
+   FOREIGN KEY (company_id) REFERENCES t_company(id)
+);
